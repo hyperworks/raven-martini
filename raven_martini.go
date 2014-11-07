a4e9a001ddbaceb88d29032fe193c85637ea9140
Matthew Kanwisher <matt@kanwisher.com>
2014-11-07 17:10:28 +0700

    send stacktraces to getsentry

--- raven_martini.go
+++ raven_martini.go
@@ -2,11 +2,17 @@ package ravenrecover
 
 import (
 		"log"
+		"fmt"
+		"net/http"
 
 		"github.com/go-martini/martini"
 		"github.com/getsentry/raven-go"
 )
 var client *raven.Client
+
+func trace() *raven.Stacktrace {
+	return raven.NewStacktrace(0, 2, nil)
+}
 
 //Send in your raven dsn should look something like this
 // "https://longnumber:lonnumber@app.getsentry.com/shortnumber"
@@ -18,15 +24,17 @@ func RecoverRaven(dsn string, logger *log.Logger) martini.Handler {
 		logger.Printf("Error loading raven -%s \n", err.Error())
 	}
 
-	return func(c martini.Context, log *log.Logger) {
+	return func(c martini.Context, log *log.Logger, req *http.Request) {
 		defer func() {
 			if e := recover(); e != nil {
 				if err, ok := e.(error); ok {
 					log.Printf("Sending this error to get sentry ")
-					client.CaptureError(err, map[string]string{})
+					packet := raven.NewPacket(err.Error(), raven.NewException(err, trace()), raven.NewHttp(req))
+					client.Capture(packet, nil)
 				} else if  strErr, ok := e.(string); ok {
 					log.Printf("Sending this error to get sentry ")
-					client.CaptureMessage(strErr, map[string]string{})
+					packet := raven.NewPacket(strErr, raven.NewException(fmt.Errorf(strErr), trace()), raven.NewHttp(req))
+					client.Capture(packet, nil)
 				}
 				panic(e)
 			}
